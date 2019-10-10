26546da41c68fdffc37ef966ee6b67470ff0ba12
Sanja <codesanja@gmail.com>
2019-10-10 17:19:19 +0100

    Building Brand page.

--- src/js/tastic/frontastic-header/tastic.jsx
+++ src/js/tastic/frontastic-header/tastic.jsx
@@ -4,7 +4,7 @@ import frontasticLogo from '../../../layout/frontasticLogo.svg'
 class FrontasticHeaderTastic extends Component {
     render () {
         return (
-            <div style={{ margin: '15px' }}>
+            <div style={{ margin: '30px 70px 0' }}>
                 <img src={frontasticLogo} alt='Frontastic Logo' />
             </div>
         )
