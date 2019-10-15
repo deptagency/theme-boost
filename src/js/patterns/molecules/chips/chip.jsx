1b7715042a8de88caf064998cdba66055e7035ee
timhartmann <hallo@timhartmann.eu>
2019-10-15 09:26:04 +0200

    refactor(chip): build useable react component

--- src/js/patterns/molecules/chips/chip.jsx
+++ src/js/patterns/molecules/chips/chip.jsx
@@ -1,27 +1,58 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import _ from 'lodash'
 
 import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
 
 class MoleculesChip extends Component {
     render () {
-        return (<div>
-            <div className='c-chip'>
-                <span>Blau</span>
-                <button className='c-chip__trigger'>
-                    <svg className='c-icon c-icon--s' version='1.1' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
-                        <title>cross</title>
-                        <path d='M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z' />
-                    </svg>
-                </button>
-            </div>
-        </div>)
+        const Component = this.props.component
+
+        return (
+          <Component
+            {..._.omit(this.props, ['children', 'component', 'className', 'active', 'selected', 'title', 'htmlType'])}
+            type={this.props.htmlType}
+            className={classnames(
+              'c-chip',
+              this.props.className,
+              this.props.active ? 'is-active' : null,
+              this.props.selected ? 'is-selected' : null,
+            )}
+          >
+              <span>{this.props.title}</span>
+              {this.props.deleteable ?
+                  <button className='c-chip__trigger' onClick={this.props.onClick}>
+                      <svg className='c-icon c-icon--s' version='1.1' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+                          <title>Delete Item</title>
+                          <path d='M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z' />
+                      </svg>
+                  </button>
+              : null }
+          </Component>
+        )
     }
 }
 
 MoleculesChip.propTypes = {
+    children: PropTypes.node.isRequired,
+    component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    className: PropTypes.string,
+    active: PropTypes.bool,
+    selected: PropTypes.bool,
+    deleteabled: PropTypes.bool,
+    title: PropTypes.string,
+    htmlType: PropTypes.string,
 }
 
 MoleculesChip.defaultProps = {
+  component: 'div',
+  className: '',
+  active: null,
+  selected: null,
+  deleteabeld: null,
+  title: "Chip",
+  htmlType: null,
 }
 
 export default ComponentInjector.return('MoleculesChip', MoleculesChip)
--- src/js/stories/molecules.chips.stories.js
+++ src/js/stories/molecules.chips.stories.js
@@ -1,71 +1,33 @@
 import React from 'react'
 
+import MoleculesChip from 'js/patterns/molecules/chips/chip'
+
 export default {
     title: '2.Molecules|Chips',
 }
 
-export const all = () => {
-    return (
-        <div>
-            <h3>Chip Default</h3>
-            <div className='c-chip'>
-                <span>Blau</span>
-                <button className='c-chip__trigger'>
-                    <svg
-                        className='c-icon c-icon--s'
-                        version='1.1'
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='24'
-                        height='24'
-                        viewBox='0 0 24 24'
-                    >
-                        <title>cross</title>
-                        <path d='M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z' />
-                    </svg>
-                </button>
-            </div>
-            <h3 className='o-distance'>Chip Selected</h3>
-            <div className='c-chip c-chip--selected'>
-                <span>Blau</span>
-                <button className='c-chip__trigger'>
-                    <svg
-                        className='c-icon c-icon--s'
-                        version='1.1'
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='24'
-                        height='24'
-                        viewBox='0 0 24 24'
-                    >
-                        <title>cross</title>
-                        <path d='M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z' />
-                    </svg>
-                </button>
-            </div>
-            <h3 className='o-distance'>Chip Active</h3>
-            <div className='c-chip is-active'>
-                <span>Blau</span>
-                <button className='c-chip__trigger'>
-                    <svg
-                        className='c-icon c-icon--s'
-                        version='1.1'
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='24'
-                        height='24'
-                        viewBox='0 0 24 24'
-                    >
-                        <title>cross</title>
-                        <path d='M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z' />
-                    </svg>
-                </button>
-            </div>
-            <h3 className='o-distance'>Chip to choose</h3>
-            <button className='c-chip'>Blau</button>
-            <h3 className='o-distance'>Active Chip to choose</h3>
-            <button className='c-chip is-active'>Blau</button>
-        </div>
-    )
+export const Deleteable = () => {
+  return (
+    <MoleculesChip deleteable="true" />
+  )
 }
-
-all.story = {
-    name: 'All',
+export const ActiveDeleteable = () => {
+  return (
+    <MoleculesChip active="true" deleteable="true" />
+  )
+}
+export const SelectedDeleteable = () => {
+  return (
+    <MoleculesChip selected="true" deleteable="true" />
+  )
+}
+export const Default = () => {
+  return (
+    <MoleculesChip component="button" />
+  )
+}
+export const Active = () => {
+  return (
+    <MoleculesChip component="button" active="true" />
+  )
 }
