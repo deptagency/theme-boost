df76a0e6fb85017e298f94b67afeb45156c5e3ca
timhartmann <hallo@timhartmann.eu>
2019-10-15 15:22:57 +0200

    add image color swiper

---
+++ src/js/patterns/organisms/swiper/image-color-swiper.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
+
+class OrganismsImageColorSwiper extends Component {
+    render () {
+        return (
+            <div>
+              <div className='o-swiper'>
+                  <div className='o-swiper__holder'>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt="" />
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                    <div className='o-swiper__item'> 
+                        <img className='o-swiper__image' src="https://mosaic04.ztat.net/vgs/media/catalog-lg/BL/82/1D/00/7K/11/BL821D007-K11@9.jpg" width="100px" alt=""/>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        )
+    }
+}
+
+OrganismsImageColorSwiper.propTypes = {
+}
+
+OrganismsImageColorSwiper.defaultProps = {
+}
+
+export default ComponentInjector.return('OrganismsImageColorSwiper', OrganismsImageColorSwiper)
---
+++ src/js/stories/organisms.swiper.stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import OrganismsImageColorSwiper from 'js/patterns/organisms/swiper/image-color-swiper'
+
+export default {
+    title: '3.Organisms|Swiper',
+}
+
+export const imageColorSwiper = () => {
+    return (
+        <OrganismsImageColorSwiper />
+    )
+}
+
+imageColorSwiper.story = {
+    name: 'Image Color Swiper',
+}
