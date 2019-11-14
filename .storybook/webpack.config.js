ef7b9489576efa70063058c196b9bbe6a67caa78
Marcel Fahle <m.fahle@gmail.com>
2019-11-14 11:44:29 +0100

    Adds svgr to boost, removes Storybook default SVG
    Storybook comes with a default SVG (and all other assets loader).
    The simple hack right now is to just modify the default loader
    regex and stick our loader in.
    
    In the future it would probably nice to put this functionality
    into a Storybook preset:
    https://github.com/storybookjs/storybook/blob/next/docs/src/pages/presets/writing-presets/index.md
    and give back to the OS community. Could be a good first
    contribution, since we're relying heavily on Storybook.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,16 +10,30 @@ module.exports = async ({ config, mode }) => {
         'node_modules'
     )
 
-    // This is specific to storybook and does not make sense to extract
-    config.module.rules.push(
-        {
-            test: /\.woff2?$|\.otf?$|\.ttf$|\.eot$/,
-            loader: 'file-loader',
+    // Don't use Storybook's default SVG Configuration
+    config.module.rules = config.module.rules.map((rule) => {
+        if (rule.test.toString().includes('svg')) {
+            const test = rule.test
+                .toString()
+                .replace('svg|', '')
+                .replace(/\//g, '')
+            return { ...rule, test: new RegExp(test) }
+        } else {
+            return rule
         }
-    )
+    })
+
+    // This is specific to storybook and does not make sense to extract
+    config.module.rules.push({
+        test: /\.woff2?$|\.otf?$|\.ttf$|\.eot$/,
+        loader: 'file-loader',
+    })
 
     config = require('../../../../catwalk/config/webpack/babel.js')(config)
     config = require('../../../../catwalk/config/webpack/loadCss.js')(config)
+    config = require('../../../../catwalk/config/webpack/svgr.js')(config)
 
+    // in case webpack config needs to be debugged
+    //return console.dir(config, { depth: null }) || config
     return config
 }
