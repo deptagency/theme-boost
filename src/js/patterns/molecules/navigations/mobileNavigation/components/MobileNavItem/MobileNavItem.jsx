7b407cf48e9a769f32d454b36a0aa377ba2f177f
Marcel Fahle <m.fahle@gmail.com>
2019-11-15 09:46:12 +0100

    MobileNavItem cleanup

--- src/js/patterns/molecules/navigations/mobileNavigation/components/MobileNavItem/MobileNavItem.jsx
+++ src/js/patterns/molecules/navigations/mobileNavigation/components/MobileNavItem/MobileNavItem.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import NodeLink from 'frontastic-catwalk/src/js/app/nodeLink.jsx'
-// TODO: that path hurts! resolve into webpack or something like that
-import forwardArrowSrc, {ReactComponent as ForwardIcon} from './../../../../../../../icons/arrow_forward.svg'
+import { ReactComponent as ForwardIcon } from './../../../../../../../icons/arrow_forward.svg'
 
-import {MobileNavTree} from '../MobileNavTree'
-import {categoryTreeType} from '../../types'
+import { MobileNavTree } from '../MobileNavTree'
+import { categoryTreeType } from '../../types'
 
-export function MobileNavItem({item, level, navPath, onClick}) {
+export function MobileNavItem({ item, level, navPath, onClick }) {
     // helper to see if the current item is part of the path.
     const isItemInPath = (item) => {
         return navPath.find((e) => {
@@ -21,15 +20,15 @@ export function MobileNavItem({item, level, navPath, onClick}) {
 
     const NextButton = () => {
         return (
-            <a
+            <button
                 onClick={() => {
                     return onClick(item, level)
                 }}
                 title='Startseite'
-                className='c-navigation__next-level-button'>
-                {/* img src={forwardArrowSrc} /> */}
+                className='c-navigation__next-level-button'
+            >
                 <ForwardIcon />
-            </a>
+            </button>
         )
     }
     return (
@@ -43,7 +42,8 @@ export function MobileNavItem({item, level, navPath, onClick}) {
                     }
                 }}
                 title='Startseite'
-                className='c-navigation__anchor'>
+                className='c-navigation__anchor'
+            >
                 {item.name}
             </NodeLink>
             {hasSubLevel(item) && <NextButton />}
