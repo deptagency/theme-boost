3e243bbe7adf00c04eb07b7e3b5f655e02a0b30e
timhartmann <hallo@timhartmann.eu>
2019-11-15 10:19:19 +0100

    sorting storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -60,6 +60,10 @@ addParameters({
 
 addParameters({
     options: {
+       storySort: (a, b) =>
+        a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, {
+           numeric: true
+        }),
         theme: create({
             base: 'light',
             brandTitle: 'Frontastic',
