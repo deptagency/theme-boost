fbbeb3f555d6e5a13652f760822e8cd0c1032fc1
Marcel Fahle <m.fahle@gmail.com>
2019-11-21 13:41:56 +0100

    Fixes Storybook loaders
    Storybook's way of extending the Webpack config (push, rather than merge) doesn't play well with our way of configuring Webpack. That's why rules never get merged or overwritten but pushed onto a stack instead.
    
    That's what happened yesterday, plain CSS files from node_modules have been processed twice: once from our loader and then again from web packs default loader. If you pluck out the absolute pathnames (webpack loader debugging in a monorepo environment sucks!) you can see the flow:
    
    ```
    css-loader/dist/cjs.js??ref--10-1!
    postcss-loader/src??postcss!
    style-loader!
    css-loader/dist/cjs.js??ref--15-1!
    postcss-loader/src??postcss!
    ventura-slider/dist/tiny-slider.css
    ```
    
    The solution was to let Storybook do the CSS processing (included by default) and just add a sass loader. Our loader, as said doesn't play well because of `wepack-merge` but Storybook has now something called presets. The `@storybook/preset-scss` works just fine but is missing one critical component: `resolve-url-loader` which let's us use static assets from inside nested sass partials.
    
    The last solution was then adding yet another sass loader, which sort of sucks but also works now. And I think it's fair to assume that storybook and the catwalk projects need to be handled slightly different.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
 
 module.exports = async ({ config, mode }) => {
     // This is specific to storybook and does not make sense to extract
     config.resolve.modules.push(
         path.resolve(__dirname, '../'),
-        path.resolve(__dirname, '../src'),
-        path.resolve(__dirname, '../../../../../node_modules'),
-        path.resolve(__dirname, '../node_modules'),
-        'node_modules'
+        path.resolve(__dirname, '../src')
+        //path.resolve(__dirname, './../../../../../node_modules'),
+        //path.resolve(__dirname, '../node_modules')
+        //'node_modules'
     )
 
     // Don't use Storybook's default SVG Configuration
@@ -23,15 +24,57 @@ module.exports = async ({ config, mode }) => {
         }
     })
 
+    config.module.rules.push({
+        test: /\.(js|jsx)$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+            cacheDirectory: true,
+            rootMode: 'upward',
+        },
+    })
+
+    /**
+     * The sass-loader is in here for two reasons:
+     *
+     * 1) `webpack-merge` doesn't work well with teh way
+     *    Storybook adds loaders (`rules.push()`) which makes
+     *    our regular config not usable.
+     * 2) There is a simple preset available (`@storybook/preset-scss`)
+     *    which works great, but unfortunately misses the `resolve-url-loader`
+     *    which we need when we want to reference assets from scss partials.
+     * */
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+    })
+
     // This is specific to storybook and does not make sense to extract
     config.module.rules.push({
         test: /\.woff2?$|\.otf?$|\.ttf$|\.eot$/,
         loader: 'file-loader',
     })
 
-    config = require('../../../../catwalk/config/webpack/babel.js')(config)
-    config = require('../../../../catwalk/config/webpack/loadCss.js')(config)
-    config = require('../../../../catwalk/config/webpack/svgr.js')(config)
+    config.module.rules.push({
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: {
+            test: /\.jsx?$/,
+        },
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                },
+            },
+            'url-loader',
+        ],
+    })
+
+    config.module.rules.push({
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['url-loader'],
+    })
 
     // in case webpack config needs to be debugged
     //return console.dir(config, { depth: null }) || config
