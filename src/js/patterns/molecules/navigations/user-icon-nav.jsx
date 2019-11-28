9fabe6c9307036dec511961c594f3b2109f9f98d
Sanja <codesanja@gmail.com>
2019-11-28 10:09:18 +0000

    My pfogile icon - desktop.

--- src/js/patterns/molecules/navigations/user-icon-nav.jsx
+++ src/js/patterns/molecules/navigations/user-icon-nav.jsx
@@ -5,12 +5,21 @@ import IconButton from '../../atoms/buttons/IconButton'
 import { ReactComponent as CartIcon } from '../../../../icons/cart.svg'
 import { ReactComponent as WishlistIcon } from '../../../../icons/wishlist-favorite.svg'
 import { ReactComponent as SearchIcon } from '../../../../icons/search.svg'
+import { ReactComponent as MyProfile } from './../../../../icons/my-profile.svg'
 
 const MoleculesUserIconNav = () => {
     return (
-        <ul className='o-list-inline c-user-icon-nav'>
+        <ul className='o-header__top-right o-list-inline c-user-icon-nav'>
             <li className='o-list-inline__item'>
                 <IconButton icon={<SearchIcon />}>Suche</IconButton>
+            </li>
+            <li className='o-list-inline__item my-profile--desktop'>
+                <IconButton
+                    icon={<MyProfile />}
+                    onClick={() => { app.getRouter().push('Frontastic.Frontend.Master.Account.profile') }}
+                >
+                    Mein Konto
+                </IconButton>
             </li>
             <li className='o-list-inline__item'>
                 <IconButton icon={<WishlistIcon />}>Wunschliste</IconButton>
