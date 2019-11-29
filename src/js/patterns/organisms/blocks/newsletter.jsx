1463cd7a37c404ad83e6f849aa10d51b34839a82
chloe0592 <klaudija.ljevar@gmail.com>
2019-11-29 15:54:09 +0100

    translations newsletter, footer, product-view

--- src/js/patterns/organisms/blocks/newsletter.jsx
+++ src/js/patterns/organisms/blocks/newsletter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { FormattedMessage } from 'react-intl'
 
 import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
 
@@ -8,15 +9,17 @@ class OrganismsNewsletterBlock extends Component {
             <div className='c-box t-spotlight'>
                 <div className='o-grid o-grid--medium-half'>
                     <div>
-                        <p className='c-topic t-text-ghost'>Exklusive Rabatte</p>
-                        <h3 className='c-title t-text-ghost'>Werde Teil unserer kleinen Familie</h3>
+                        <p className='c-topic t-text-ghost'><FormattedMessage id='exlusiveDiscounts'/></p>
+                        <h3 className='c-title t-text-ghost'><FormattedMessage id='becomeFamily'/></h3>
                     </div>
                     <div>
                         <form method='' action=''>
-                            <input type='email' placeholder='Deine E-Mail Adresse' className='c-input-ghost' />
+                            <FormattedMessage id='yourEmail' >
+                                {placeholder => <input type='email' placeholder={placeholder} className='c-input-ghost' />}
+                            </FormattedMessage>
                             <div className='o-distance-s'>
-                                <button type='submit' className='c-button c-button--boss c-button--ghost'>Absenden</button>
-                                <p className='u-text-xs o-distance-s'>Mit dem Klicken auf ‚Absenden’ stimmst du zu, dass wir deine Informationen im Rahmen unserer Datenschutzbestimmungen verarbeiten.</p>
+                                <button type='submit' className='c-button c-button--boss c-button--ghost'><FormattedMessage id='submit'/></button>
+                                <p className='u-text-xs o-distance-s'><FormattedMessage id='submitAgree'/></p>
                             </div>
                         </form>
                     </div>
