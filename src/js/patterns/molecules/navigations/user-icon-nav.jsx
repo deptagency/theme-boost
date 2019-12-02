32e45d1e9c9eb7c527654d4605bddc58080078a1
chloe0592 <klaudija.ljevar@gmail.com>
2019-12-02 13:44:26 +0100

    added translation for header

--- src/js/patterns/molecules/navigations/user-icon-nav.jsx
+++ src/js/patterns/molecules/navigations/user-icon-nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import app from 'frontastic-catwalk/src/js/app/app'
+import { FormattedMessage } from 'react-intl'
 
 import IconButton from '../../atoms/buttons/IconButton'
 import { ReactComponent as CartIcon } from '../../../../icons/cart.svg'
@@ -11,25 +12,27 @@ const MoleculesUserIconNav = () => {
     return (
         <ul className='o-header__top-right o-list-inline c-user-icon-nav'>
             <li className='o-list-inline__item'>
-                <IconButton icon={<SearchIcon />}>Suche</IconButton>
+                <IconButton icon={<SearchIcon />}>
+                    <FormattedMessage id='header.search' />
+                </IconButton>
             </li>
             <li className='o-list-inline__item my-profile--desktop'>
                 <IconButton
                     icon={<MyProfile />}
-                    onClick={() => { app.getRouter().push('Frontastic.Frontend.Master.Account.profile') }}
-                >
-                    Mein Konto
+                    onClick={() => { app.getRouter().push('Frontastic.Frontend.Master.Account.profile') }}>
+                        <FormattedMessage id='header.myAccount' />
                 </IconButton>
             </li>
             <li className='o-list-inline__item'>
-                <IconButton icon={<WishlistIcon />}>Wunschliste</IconButton>
+                <IconButton icon={<WishlistIcon />}>
+                    <FormattedMessage id='header.wishlist' />
+                </IconButton>
             </li>
             <li className='o-list-inline__item'>
                 <IconButton
                     icon={<CartIcon />}
-                    onClick={() => { app.getRouter().push('Frontastic.Frontend.Master.Checkout.cart') }}
-                >
-                    Warenkorb
+                    onClick={() => { app.getRouter().push('Frontastic.Frontend.Master.Checkout.cart') }}>
+                        <FormattedMessage id='header.cart' />
                 </IconButton>
             </li>
         </ul>
