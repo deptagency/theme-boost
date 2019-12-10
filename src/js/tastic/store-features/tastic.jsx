9f8f0c1b15ce307cc01435b2b15d2023986c3ab6
Marcel Fahle <m.fahle@gmail.com>
2019-12-10 13:14:47 +0100

    adds store features tastic

---
+++ src/js/tastic/store-features/tastic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import StoreFeatureList from '../../patterns/molecules/StoreFeatureList'
+
+function StoreFeatures() {
+    return <StoreFeatureList />
+}
+
+export default StoreFeatures
