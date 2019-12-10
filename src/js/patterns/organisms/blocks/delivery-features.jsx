ecd2a01f3df8f671b9fc26e7f663dcd6029ae1ce
Marcel Fahle <m.fahle@gmail.com>
2019-12-10 18:37:15 +0100

    Removes static component of store features
    there is a new one with dynamic content,
    that will replace this one. should be deleted soon.

--- src/js/patterns/organisms/blocks/delivery-features.jsx
+++ src/js/patterns/organisms/blocks/delivery-features.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react'
-import OrganismsFeatureListing from '../listings/feature-listing'
+//import OrganismsFeatureListing from '../listings/feature-listing'
 import OrganismsExpressDelivery from './express-delivery'
+
+// NOTE: this pattern and the tastic (delivery-details/tastic.jsx)
+// are most definitely obsolete. *marcel
 
 const OrganismsDeliveryFeatures = () => {
     return (
         <Fragment>
             <OrganismsExpressDelivery />
-            <OrganismsFeatureListing />
+            {/*<OrganismsFeatureListing />*/}
         </Fragment>
     )
 }
