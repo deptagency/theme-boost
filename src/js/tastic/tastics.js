e7e810d1e623f418b0cee9f477e5caba28b682b2
Sanja <codesanja@gmail.com>
2019-12-11 17:55:16 +0000

    Removing tastics that are not customizable for now.

--- src/js/tastic/tastics.js
+++ src/js/tastic/tastics.js
@@ -1,49 +1,19 @@
 import HeaderTastic from './header/tastic.jsx'
-import FooterTastic from './footer/tastic.jsx'
-import HeroTastic from './hero/tastic.jsx'
-import CategoryListingTastic from './category-listing/tastic.jsx'
 import PromoBoxTastic from './promo-box/tastic.jsx'
-import CategoryBoxesTastic from './category-boxes/tastic.jsx'
 import CategoryButtonBoxesTastic from './category-button-boxes/tastic.jsx'
 import ProductSliderTastic from './product-slider/tastic.jsx'
 import NewsletterTastic from './newsletter/tastic'
-import ProductStageTastic from './product-stage/tastic'
-import SizeFilterTastic from './size-filter/tastic'
-import AddToCartButtonTastic from './add-to-cart-button/tastic'
 import MoreInfoProductTastic from './more-info-product/tastic'
 import BrandContentfulTastic from './brand-contentful/tastic'
 import HorizontalSpacerTastic from './horisontal-spacer/tastic'
 import FrontasticHeaderTastic from './frontastic-header/tastic'
-import CheckOrderTastic from './check-order/tastic'
-import OrderConfirmationTastic from './order-confirmation/tastic'
 import DeliveryFeaturesTastic from './delivery-details/tastic'
-import DirectDebitLoginTastic from './direct-debit-login/tastic'
-import AddAddressTastic from './add-address/tastic'
-import AddAddressOnCheckoutTastic from './add-address-on-checkout/tastic'
-import CreditCardTastic from './credit-card/tastic'
-import ProductColorFilterTastic from './product-color-filter/tastic'
 import ChangeEmailTastic from './change-email/tastic'
 import ChangeAddressTastic from './change-address/tastic'
-import ChangePasswordTastic from './change-password/tastic'
-import HelpSectionTastic from './help-section/tastic'
 import CheckoutTastic from './checkout/tastic'
 import ProductViewTastic from './product-view/tastic'
-import TemplatesOrderBilling from './order-billing/tastic'
-import DirectDebitTastic from './direct-debit/tastic'
 import CartTastic from './cart/tastic'
-import GuideBrandTastic from './guide-brand/tastic'
-import GuideMaterialTastic from './guide-material/tastic'
-import GuidePatternsTastic from './guide-patterns/tastic'
-import PasswordResetTastic from './password-reset/tastic'
-import UserRegistrationFormTastic from './user-registration-form/tastic'
-import UserRegistrationOnCheckoutFormTastic from './user-registration-on-checkout-form/tastic'
-import SortTastic from './sort/tastic'
-import ChangeEmailFromProfileTastic from './change-email-from-profile/tastic'
-import ChangeAddressFromProfileTastic from './change-address-from-profile/tastic'
-import ProfileTastic from './profile/tastic'
-import FilterTastic from './filter/tastic'
 import LoginTastic from './login/tastic'
-import MyOrders from './my-orders/tastic'
 import ShowcaseTextTastic from './showcase-text/tastic'
 import AccountAccessTastic from './account/access/tastic'
 import AccountConfirmTastic from './account/confirm/tastic'
@@ -58,51 +28,21 @@ import FooterSocialNavTastic from './footer-social-nav/tastic'
 export default (() => {
     return {
         'frontastic/boost/header': HeaderTastic,
-        'frontastic/boost/footer': FooterTastic,
-        'frontastic/boost/hero': HeroTastic,
-        'frontastic/boost/category-listing': CategoryListingTastic,
         'frontastic/boost/promobox': PromoBoxTastic,
-        'frontastic/boost/category-boxes': CategoryBoxesTastic,
         'frontastic/boost/category-button-boxes': CategoryButtonBoxesTastic,
         'frontastic/boost/product-slider': ProductSliderTastic,
         'frontastic/boost/newsletter': NewsletterTastic,
-        'frontastic/boost/product-stage': ProductStageTastic,
-        'frontastic/boost/size-filter': SizeFilterTastic,
-        'frontastic/boost/add-to-cart-button': AddToCartButtonTastic,
         'frontastic/boost/more-info-product': MoreInfoProductTastic,
         'frontastic/boost/brand-contentful': BrandContentfulTastic,
         'frontastic/boost/horizontal-spacer': HorizontalSpacerTastic,
         'frontastic/boost/frontastic-header': FrontasticHeaderTastic,
-        'frontastic/boost/check-order': CheckOrderTastic,
-        'frontastic/boost/order-confirmation': OrderConfirmationTastic,
         'frontastic/boost/delivery-features': DeliveryFeaturesTastic,
-        'frontastic/boost/direct-debit-login': DirectDebitLoginTastic,
-        'frontastic/boost/add-address': AddAddressTastic,
-        'frontastic/boost/add-address-on-checkout': AddAddressOnCheckoutTastic,
-        'frontastic/boost/credit-card': CreditCardTastic,
-        'frontastic/boost/product-color-filter': ProductColorFilterTastic,
         'frontastic/boost/change-email': ChangeEmailTastic,
         'frontastic/boost/change-address': ChangeAddressTastic,
-        'frontastic/boost/change-password': ChangePasswordTastic,
-        'frontastic/boost/help-section': HelpSectionTastic,
         'frontastic/boost/checkout': CheckoutTastic,
         'frontastic/boost/product-view': ProductViewTastic,
-        'frontastic/boost/order-billing': TemplatesOrderBilling,
-        'frontastic/boost/direct-debit': DirectDebitTastic,
         'frontastic/boost/cart': CartTastic,
-        'frontastic/boost/guide-brand': GuideBrandTastic,
-        'frontastic/boost/guide-material': GuideMaterialTastic,
-        'frontastic/boost/guide-patterns': GuidePatternsTastic,
-        'frontastic/boost/password-reset': PasswordResetTastic,
-        'frontastic/boost/user-registration-form': UserRegistrationFormTastic,
-        'frontastic/boost/user-registration-on-checkout-form': UserRegistrationOnCheckoutFormTastic,
-        'frontastic/boost/sort': SortTastic,
-        'frontastic/boost/change-email-from-profile': ChangeEmailFromProfileTastic,
-        'frontastic/boost/change-address-from-profile': ChangeAddressFromProfileTastic,
-        'frontastic/boost/profile': ProfileTastic,
-        'frontastic/boost/filter': FilterTastic,
         'frontastic/boost/login': LoginTastic,
-        'frontastic/boost/my-orders': MyOrders,
         'frontastic/boost/showcase-text': ShowcaseTextTastic,
         'frontastic/boost/account-access': AccountAccessTastic,
         'frontastic/boost/account-confirm': AccountConfirmTastic,
