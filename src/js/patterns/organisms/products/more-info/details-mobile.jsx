301612a4499c646ec2bd82b8888817088475776a
Tobias Schlitt <toby@php.net>
2019-12-13 11:53:26 +0100

    Adjusted product details to demo model and made it more resilient.
    The resilience allows use to use the tastic in shops with other product
    models without breaking the site.

--- src/js/patterns/organisms/products/more-info/details-mobile.jsx
+++ src/js/patterns/organisms/products/more-info/details-mobile.jsx
@@ -1,25 +1,43 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 
 const DetailsMobile = ({ variant }) => {
     return <div className='o-distance'>
         <dl className='o-detaillist o-distance-m'>
-            <dt className='t-text-quiet'><FormattedMessage id='product.materialOuterFabric'/></dt>
-            <dd className='u-text-strong'>100% <FormattedMessage id='product.viscose'/></dd>
-            <dt className='t-text-quiet'><FormattedMessage id='product.lining'/></dt>
-            <dd className='u-text-strong'>100% <FormattedMessage id='product.viscose'/></dd>
+            {variant.attributes.color && variant.attributes.color.label ?
+                <Fragment>
+                    <dt className='t-text-quiet'><FormattedMessage id='product.color'/></dt>
+                    <dd className='u-text-strong'>{variant.attributes.color.label}</dd>
+                </Fragment>
+            : null}
+            {variant.attributes.designer && variant.attributes.designer.label ?
+                <Fragment>
+                    <dt className='t-text-quiet'><FormattedMessage id='product.designer'/></dt>
+                    <dd className='u-text-strong'>{variant.attributes.designer.label}</dd>
+                </Fragment>
+            : null}
         </dl>
         <h3 className='c-title-level-3 o-distance'><FormattedMessage id='product.moreAboutThisProduct'/></h3>
         <dl className='o-detaillist o-distance-m'>
-            <dt className='t-text-quiet'><FormattedMessage id='product.cut'/></dt>
-            <dd className='u-text-strong'>100% <FormattedMessage id='product.viscose'/></dd>
-            <dt className='t-text-quiet'><FormattedMessage id='product.clasp'/></dt>
-            <dd className='u-text-strong'><FormattedMessage id='product.lacing'/></dd>
-            <dt className='t-text-quiet'><FormattedMessage id='product.style'/></dt>
-            <dd className='u-text-strong'>{variant.attributes.style.label}</dd>
-            <dt className='t-text-quiet'><FormattedMessage id='product.itemNumber'/></dt>
-            <dd className='u-text-strong'>{variant.attributes.matrixId}</dd>
+            {variant.attributes.style && variant.attributes.style.label ?
+                <Fragment>
+                    <dt className='t-text-quiet'><FormattedMessage id='product.style'/></dt>
+                    <dd className='u-text-strong'>{variant.attributes.style.label}</dd>
+                </Fragment>
+            : null}
+            {variant.attributes.commonSize && variant.attributes.commonSize.label ?
+                <Fragment>
+                    <dt className='t-text-quiet'><FormattedMessage id='product.commonSize'/></dt>
+                    <dd className='u-text-strong'>{variant.attributes.commonSize.label}</dd>
+                </Fragment>
+            : null}
+            {variant.attributes.matrixId ?
+                <Fragment>
+                    <dt className='t-text-quiet'><FormattedMessage id='product.itemNumber'/></dt>
+                    <dd className='u-text-strong'>{variant.attributes.matrixId}</dd>
+                </Fragment>
+            : null}
         </dl>
     </div>
 }
