3b1d23a0bdcde9bc263e601ea3b9d3577a35899d
chloe0592 <klaudija.ljevar@gmail.com>
2019-12-18 08:50:18 +0100

    ribbon beta added to homepage

--- src/js/patterns/molecules/promobox/promobox-with-positioning.jsx
+++ src/js/patterns/molecules/promobox/promobox-with-positioning.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Image from '@frontastic/catwalk/src/js/image'
 import ComponentInjector from '@frontastic/catwalk/src/js/app/injector'
 import Translatable from '@frontastic/catwalk/src/js/component/translatable'
+import './promobox-with-positioning.scss'
 
 class OrganismsPromoboxWithPositioning extends Component {
     render () {
@@ -18,6 +19,11 @@ class OrganismsPromoboxWithPositioning extends Component {
             return null
         }
 
+        var showRibbon = false;
+        if (vertical == 'top') {
+            showRibbon = true;
+        }
+
         return (
             <div className='c-promobox t-spotlight u-aspect-ratio-16x9' style={style}>
                 <Image
@@ -25,6 +31,11 @@ class OrganismsPromoboxWithPositioning extends Component {
                     alt={<Translatable value={image.media.title} />}
                     className='c-promobox__backdrop tns-lazy' />
                 <div className={`c-promobox__overlay c-promobox__overlay--${vertical}-${horizontal}`}>
+                    {showRibbon && (
+                        <div className='ribbonTopRight ribbon'>
+                            <span>BETA</span>
+                        </div>
+                    )}
                     <div className={`c-promobox__overlay__inner`}>
                         <div style={customChildrenStyle}>
                             {children}
