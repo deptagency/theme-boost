b037153b8134d92f0171a1db40da71f1f5c28890
timhartmann <hallo@timhartmann.eu>
2019-12-21 06:35:49 +0100

    remove television checkout-login and -register

--- src/js/patterns/templates/checkout-login.jsx
+++ src/js/patterns/templates/checkout-login.jsx
@@ -8,30 +8,26 @@ import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
 class TemplatesLogin extends Component {
     render () {
         return (
-            <div className='o-television'>
+            <>
                 <OrganismsHeaderMobile />
-                <div className='o-television__display'>
-                    <div className='o-container-small'>
-                        <OrganismsPageHeader title='Anmelden' />
-                        <h2 className='c-title-level-3 o-distance-m'>Melde dich an oder erstelle ein Konto</h2>
-                        <div className='o-buttonbar o-distance-s'>
-                            <Button type='primary' size='boss' className='o-buttonbar__item'>Anmelden</Button>
-                            <Button type='primary' size='boss' className='o-buttonbar__item'>Registrieren</Button>
-                        </div>
-                        <div className='o-television__display__boundless'>
-                            <span className='c-divider c-divider--break o-distance-m' />
-                        </div>
-                        <section className='o-distance-m'>
-                            <h2 className='c-title-level-3'>Als Gast fortfahren</h2>
-                            <form action='' className='o-distance-s'>
-                                <label htmlFor='' className='c-label'>E-Mail-Adresse</label>
-                                <input type='email' name='' id='' className='o-distance-xs' />
-                            </form>
-                            <Button type='primary' size='boss' className='o-distance-m'>Als Gast zur Kasse</Button>
-                        </section>
+                <div className='o-container-small'>
+                    <OrganismsPageHeader title='Anmelden' />
+                    <h2 className='c-title-level-3 o-distance-m'>Melde dich an oder erstelle ein Konto</h2>
+                    <div className='o-buttonbar o-distance-s'>
+                        <Button type='primary' size='boss' className='o-buttonbar__item'>Anmelden</Button>
+                        <Button type='primary' size='boss' className='o-buttonbar__item'>Registrieren</Button>
                     </div>
+                    <span className='c-divider c-divider--break o-distance-m' />
+                    <section className='o-distance-m'>
+                        <h2 className='c-title-level-3'>Als Gast fortfahren</h2>
+                        <form action='' className='o-distance-s'>
+                            <label htmlFor='' className='c-label'>E-Mail-Adresse</label>
+                            <input type='email' name='' id='' className='o-distance-xs' />
+                        </form>
+                        <Button type='primary' size='boss' className='o-distance-m'>Als Gast zur Kasse</Button>
+                    </section>
                 </div>
-            </div>
+            </>
         )
     }
 }
--- src/js/patterns/templates/checkout-register.jsx
+++ src/js/patterns/templates/checkout-register.jsx
@@ -10,25 +10,19 @@ import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
 class TemplatesCheckoutRegister extends Component {
     render () {
         return (
-            <Fragment>
-                <div className='o-television'>
-                    <OrganismsHeaderMobile />
-                    <div className='o-television__display'>
-                        <div className='o-container-small'>
-                            <OrganismsFormRegister />
-                            <Button type='primary' size='boss' className='o-distance-m'>Jetzt Registrieren</Button>
-                            <div className='o-television__display__boundless'>
-                                <span className='c-divider c-divider--break o-distance-m' />
-                            </div>
-                            <section className='o-distance-m'>
-                                <h2 className='c-title-level-3 u-text-center'>Melde dich jetzt an</h2>
-                                <Button type='quiet' size='boss' className='o-distance-m'>Anmelden</Button>
-                            </section>
-                        </div>
-                        <OrganismsFooter />
-                    </div>
+            <>
+                <OrganismsHeaderMobile />
+                <div className='o-container-small'>
+                    <OrganismsFormRegister />
+                    <Button type='primary' size='boss' className='o-distance-m'>Jetzt Registrieren</Button>
+                    <span className='c-divider c-divider--break o-distance-m' />
+                    <section className='o-distance-m'>
+                        <h2 className='c-title-level-3 u-text-center'>Melde dich jetzt an</h2>
+                        <Button type='quiet' size='boss' className='o-distance-m'>Anmelden</Button>
+                    </section>
                 </div>
-            </Fragment>
+                <OrganismsFooter />
+            </>
         )
     }
 }
