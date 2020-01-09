20fc0627125fb2552469c34baa5495d0464dda0e
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-01-09 11:02:47 +0100

    added responsiveness to full cart loader

--- src/js/patterns/organisms/loaders/full-cart-loader.jsx
+++ src/js/patterns/organisms/loaders/full-cart-loader.jsx
@@ -2,29 +2,59 @@ import React, { Component } from 'react'
 
 import ComponentInjector from 'frontastic-catwalk/src/js/app/injector'
 import ContentLoader from 'react-content-loader'
+import { smallScreen, largeScreen, blockSmallScreenHide } from './loaders.module.scss'
 
 class FullCartLoader extends Component {
     render () {
         return (<div>
             <ContentLoader
-                height={400}
+                height={950}
                 width={800}
                 speed={2}
                 primaryColor='#d9d9d9'
                 secondaryColor='#787878'
                 >
-                <rect x='40' y='15' rx='20' ry='20' width='150' height='200' />
-                <rect x='40' y='320' rx='5' ry='5' width='45' height='35' />
-                <rect x='100' y='320' rx='5' ry='5' width='45' height='35' />
-                <rect x='160' y='320' rx='5' ry='5' width='45' height='35' />
-                <rect x='220' y='320' rx='5' ry='5' width='45' height='35' />
-                <rect x='220' y='17' rx='10' ry='10' width='290' height='18' />
-                <rect x='220' y='50' rx='10' ry='10' width='233' height='12' />
-                <rect x='220' y='110' rx='5' ry='5' width='165' height='8' />
-                <rect x='220' y='130' rx='5' ry='5' width='165' height='8' />
-                <rect x='220' y='180' rx='8' ry='8' width='100' height='29' />
-                <rect x='550' y='15' rx='20' ry='20' width='220' height='200' />
+                <rect className={largeScreen} x='40' y='15' rx='20' ry='20' width='150' height='200' />
 
+                <rect className={largeScreen} x='220' y='17' rx='10' ry='10' width='290' height='18' />
+
+                <rect className={largeScreen} x='220' y='50' rx='10' ry='10' width='233' height='12' />
+
+                <rect className={largeScreen} x='220' y='110' rx='5' ry='5' width='165' height='8' />
+                <rect className={largeScreen} x='220' y='130' rx='5' ry='5' width='165' height='8' />
+
+                <rect className={largeScreen} x='220' y='180' rx='8' ry='8' width='100' height='29' />
+                <rect className={largeScreen} x='550' y='15' rx='20' ry='20' width='220' height='200' />
+
+                <rect className={largeScreen} x='40' y='320' rx='5' ry='5' width='45' height='35' />
+                <rect className={largeScreen} x='100' y='320' rx='5' ry='5' width='45' height='35' />
+                <rect className={largeScreen} x='160' y='320' rx='5' ry='5' width='45' height='35' />
+                <rect className={largeScreen} x='220' y='320' rx='5' ry='5' width='45' height='35' />
+
+                {/* small screen */}
+
+                <rect className={smallScreen} x='200' y='15' rx='20' ry='20' width='150' height='200' />
+                <rect className={smallScreen} x='630' y='17' rx='2' ry='2' width='20' height='18' />
+                <rect className={smallScreen} x='400' y='17' rx='10' ry='10' width='170' height='16' />
+                <rect className={smallScreen} x='400' y='50' rx='10' ry='10' width='135' height='12' />
+                <rect className={smallScreen} x='400' y='110' rx='5' ry='5' width='120' height='8' />
+                <rect className={smallScreen} x='400' y='130' rx='5' ry='5' width='120' height='8' />
+                <rect className={smallScreen} x='400' y='180' rx='8' ry='8' width='70' height='29' />
+
+                <rect className={smallScreen} x='200' y='245' rx='20' ry='20' width='150' height='200' />
+                <rect className={smallScreen} x='630' y='247' rx='2' ry='2' width='20' height='18' />
+                <rect className={smallScreen} x='400' y='247' rx='10' ry='10' width='170' height='16' />
+                <rect className={smallScreen} x='400' y='280' rx='10' ry='10' width='135' height='12' />
+                <rect className={smallScreen} x='400' y='340' rx='5' ry='5' width='120' height='8' />
+                <rect className={smallScreen} x='400' y='380' rx='5' ry='5' width='120' height='8' />
+                <rect className={smallScreen} x='400' y='410' rx='8' ry='8' width='70' height='29' />
+
+                <rect className={smallScreen} x='200' y='550' rx='5' ry='5' width='60' height='35' />
+                <rect className={smallScreen} x='280' y='550' rx='5' ry='5' width='60' height='35' />
+                <rect className={smallScreen} x='360' y='550' rx='5' ry='5' width='60' height='35' />
+                <rect className={smallScreen} x='440' y='550' rx='5' ry='5' width='60' height='35' />
+
+                <rect className={smallScreen} x='200' y='650' rx='20' ry='20' width='450' height='200' />
             </ContentLoader>
         </div>)
     }
--- src/js/patterns/organisms/loaders/homepage-loader.jsx
+++ src/js/patterns/organisms/loaders/homepage-loader.jsx
@@ -26,25 +26,26 @@ class HomepageLoader extends Component {
                 <rect className={largeScreen} x="287" y="470" rx="7" ry="7" width="230" height="170" />
                 <rect className={largeScreen} x="547" y="470" rx="7" ry="7" width="230" height="170" />
 
-                <rect className={smallScreen} x="20" y="470" rx="7" ry="7" width="300" height="200" />
-                <rect className={smallScreen} x="340" y="470" rx="7" ry="7" width="300" height="200" />
-                <rect className={smallScreen} x="660" y="470" rx="7" ry="7" width="300" height="200" />
-
                 <rect className={largeScreen} x='200' y='660' rx='4' ry='4' width='400' height='12' />
                 <rect className={largeScreen} x='300' y='685' rx='4' ry='4' width='200' height='15' />
-
-                <rect className={smallScreen} x='200' y='690' rx='4' ry='4' width='400' height='12' />
-                <rect className={smallScreen} x='300' y='720' rx='4' ry='4' width='200' height='15' />
 
                 <rect className={largeScreen} x='20' y='720' rx='7' ry='7' width='165' height='210' />
                 <rect className={largeScreen} x='217' y='720' rx='7' ry='7' width='165' height='210' />
                 <rect className={largeScreen} x='412' y='720' rx='7' ry='7' width='165' height='210' />
                 <rect className={largeScreen} x='607' y='720' rx='7' ry='7' width='165' height='210' />
 
+                {/* small screen */}
+
+                <rect className={smallScreen} x="20" y="470" rx="7" ry="7" width="300" height="200" />
+                <rect className={smallScreen} x="340" y="470" rx="7" ry="7" width="300" height="200" />
+                <rect className={smallScreen} x="660" y="470" rx="7" ry="7" width="300" height="200" />
+                
+                <rect className={smallScreen} x='200' y='690' rx='4' ry='4' width='400' height='12' />
+                <rect className={smallScreen} x='300' y='720' rx='4' ry='4' width='200' height='15' />
+
                 <rect className={smallScreen} x='20' y='750' rx='7' ry='7' width='240' height='210' />
                 <rect className={smallScreen} x='280' y='750' rx='7' ry='7' width='240' height='210' />
                 <rect className={smallScreen} x='540' y='750' rx='7' ry='7' width='240' height='210' />
-
             </ContentLoader>
         </div>)
     }
