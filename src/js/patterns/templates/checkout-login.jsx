78231f7d0d969151e9597335e0911a1f952397c6
timhartmann <hallo@timhartmann.eu>
2020-01-10 06:26:26 +0100

    update button handling

--- src/js/patterns/templates/checkout-login.jsx
+++ src/js/patterns/templates/checkout-login.jsx
@@ -13,15 +13,17 @@ class Login extends Component {
                 <div className='o-container-small'>
                     <PageHeader title='Anmelden' />
                     <h2 className='c-title-level-3 o-distance-m'>Melde dich an oder erstelle ein Konto</h2>
-                    <div className='o-buttonbar o-distance-s'>
-                        <Button type='primary' size='boss' className='o-buttonbar__item'>Anmelden</Button>
-                        <Button type='primary' size='boss' className='o-buttonbar__item'>Registrieren</Button>
+                    <div className='o-distance-s'>
+                        <div className='o-buttonbar'>
+                            <Button type='primary' className='o-buttonbar__item o-buttonbar__item--full'>Anmelden</Button>
+                            <Button type='primary' className='o-buttonbar__item o-buttonbar__item--full'>Registrieren</Button>
+                        </div>
                     </div>
-                    <span className='c-divider c-divider--break o-distance-l' />
+                    <span className='c-divider c-divider--break o-distance-l u-hidden-large-up' />
                     <section className='o-distance-l'>
                         <h2 className='c-title-level-3'>Als Gast fortfahren</h2>
                         <form action='' className='o-distance-s'>
-                            <label htmlFor='' className='c-label'>E-Mail-Adresse</label>
+                            <label htmlFor='' className='c-form-label'>E-Mail-Address</label>
                             <input type='email' name='' id='' className='o-distance-xs' />
                         </form>
                         <Button type='primary' size='boss' className='o-distance-m'>Als Gast zur Kasse</Button>
