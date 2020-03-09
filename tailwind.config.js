ef25c89bb4f3e739ec3a132f6b8898f3c092a0e3
Sanja <codesanja@gmail.com>
2020-03-09 16:53:30 +0000

    Adding utility classes as a plugin.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
     theme: {
         extend: {
@@ -5,5 +7,20 @@ module.exports = {
                 'fix-240px': '240px',
             }
         }
-    }
+    },
+    plugins: [
+        plugin(function({ addUtilities }) {
+            const gridJustifySelf = {
+                '.justify-self-center': {
+                    justifySelf: 'center'
+                },
+                '.justify-self-end': {
+                    justifySelf: 'end'
+                },
+
+            }
+
+            addUtilities(gridJustifySelf)
+        })
+    ]
 }
