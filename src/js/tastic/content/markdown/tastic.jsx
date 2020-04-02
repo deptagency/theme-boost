d11f56b4f6ab4a1cbd20532661ae58951583ca1f
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-04-02 14:24:02 +0200

    yarn fix

--- src/js/tastic/content/markdown/tastic.jsx
+++ src/js/tastic/content/markdown/tastic.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 import Markdown from '@frontastic/catwalk/src/js/component/markdown'
 
 const MarkdownTastic = ({ data }) => {
-        return <Markdown
-            text={data.text}
-            className={classnames(
+    return <Markdown
+        text={data.text}
+        className={classnames(
                 's-text',
                 'c-markdown',
                 'c-markdown--align-' + data.align,
                 'c-markdown--padding-' + data.padding
             )}
         />
-    }
+}
 
 MarkdownTastic.propTypes = {
     data: PropTypes.object.isRequired,
