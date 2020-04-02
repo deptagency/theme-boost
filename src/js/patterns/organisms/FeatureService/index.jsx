eaa27d16f3a62b3278e28b42dd333999ed92bed1
Sanja <codesanja@gmail.com>
2020-04-02 19:34:11 +0100

    Css rework

--- src/js/patterns/organisms/FeatureService/index.jsx
+++ src/js/patterns/organisms/FeatureService/index.jsx
@@ -7,20 +7,20 @@ import { ReactComponent as IconLockClosed } from 'Icons/tailwind-icons/icon-lock
 
 const FeatureService = () => {
     return (
-        <div className='flex flex-col md:flex-row'>
-            <div className='w-full h-12 px-4 flex items-center md:justify-center border-b border-solid md:border-b-0 md:border-r'>
+        <div className='flex flex-col md:flex-row max-w-840px mx-auto'>
+            <div className='w-full h-12 flex items-center border-b border-solid md:border-b-0 md:border-r'>
                 <IconRocket className='inline-block mr-3 w-5 h-5 md:w-6 md:h-6 fill-current text-indigo-600' />
                 <span className='text-sm text-gray-800 leading-tight'>
                     Free & fast delivery
                 </span>
             </div>
-            <div className='w-full h-12 px-4 flex items-center md:justify-center border-b border-solid md:border-b-0 md:border-r'>
+            <div className='w-full h-12 flex md:justify-center items-center border-b border-solid md:border-b-0 md:border-r'>
                 <IconRefresh className='inline-block mr-3 w-5 h-5 md:w-6 md:h-6 fill-current text-indigo-600' />
                 <span className='text-sm text-gray-800 leading-tight'>
                     Free returns
                 </span>
             </div>
-            <div className='w-full h-12 px-4 flex items-center md:justify-center md:border-b-0'>
+            <div className='w-full h-12 flex md:justify-end items-center md:border-b-0'>
                 <IconLockClosed className='inline-block mr-3 w-5 h-5 md:w-6 md:h-6 fill-current text-indigo-600' />
                 <span className='text-sm text-gray-800 leading-tight'>
                     Safe payment and data protection
