a2a751a79fc1778a9222b4b2268e5bd7f3014928
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-04-02 10:39:06 +0200

    added markdown in tastics.js

--- src/js/tastic/tastics.js
+++ src/js/tastic/tastics.js
@@ -1,11 +1,13 @@
 import ShowcaseTextTastic from './content/showcase-text/tastic'
 import NewsletterBlockTastic from './forms/newsletter-block/tastic'
 import MainMenuTastic from './header/main-menu/tastic'
+import MarkdownTastic from './content/markdown/tastic'
 
 export default (() => {
     return {
         'content--showcase-text': ShowcaseTextTastic,
         'forms--newsletter-block': NewsletterBlockTastic,
         'header--main-menu': MainMenuTastic,
+        'content--markdown': MarkdownTastic,
     }
 })()
