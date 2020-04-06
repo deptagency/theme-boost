47521894c62baad00d60175742a7df647651446a
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-04-06 14:15:02 +0200

    boost 174 - margin header alignment

--- src/js/patterns/molecules/Tile/index.jsx
+++ src/js/patterns/molecules/Tile/index.jsx
@@ -36,7 +36,7 @@ const Tile = ({
 
     return (
         <Component
-            className='grid grid-cols-1 grid-rows-1 rounded-lg max-h-288px lg:max-h-456px lg:h-full overflow-hidden py-2'
+            className='grid grid-cols-1 grid-rows-1 max-h-288px lg:max-h-456px lg:h-full overflow-hidden pb-2'
             reference={reference || {
                 type: null,
                 target: null,
