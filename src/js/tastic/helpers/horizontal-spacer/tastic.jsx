0e3076518ba82f533a8fd44c1ed870cdab54b5d3
Kirill Klimenko <klimenkomud@gmail.com>
2020-04-08 18:16:51 +0300

    Refactor Horizontal Spacer to use more human-understandable size descriptions, but leave ability to have just space in px

--- src/js/tastic/helpers/horizontal-spacer/tastic.jsx
+++ src/js/tastic/helpers/horizontal-spacer/tastic.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class HorizontalSpacerTastic extends Component {
-    render () {
-        const spaceInPx = this.props.tastic.schema.get('spaceInPx') || 24
+const SIZE_MAP = {
+    xxs: '4px',
+    xs: '8px',
+    sm: '12px',
+    md: '16px',
+    lg: '20px',
+    xl: '24px',
+}
 
-        return (
-            <div
-                className='horisontal-spacer-tastic'
-                style={{
-                width: '100%',
-                height: `${spaceInPx}px`,
-            }} />
-        )
-    }
+export default function HorizontalSpacerTastic ({ tastic }) {
+    const size = tastic.schema.get('size') || 'xl'
+    const spaceInPx = tastic.schema.get('spaceInPx') || SIZE_MAP[size]
+    return (
+        <div
+            className='horisontal-spacer-tastic'
+            style={{
+            width: '100%',
+            height: `${spaceInPx}px`,
+        }} />
+    )
 }
 
 HorizontalSpacerTastic.propTypes = {
     tastic: PropTypes.object.isRequired,
 }
-
-HorizontalSpacerTastic.defaultProps = {
-}
-
-export default HorizontalSpacerTastic
