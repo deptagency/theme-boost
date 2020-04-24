a12bd38df7f0b375bc21da2460ed3dbf26b282ab
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-04-24 12:29:11 +0200

    yarn fix

--- src/js/patterns/molecules/CheckoutHeader/index.jsx
+++ src/js/patterns/molecules/CheckoutHeader/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { ReactComponent as IconArrowLeft} from 'Icons/tailwind-icons/icon-arrow-left.svg'
-
+import { ReactComponent as IconArrowLeft } from 'Icons/tailwind-icons/icon-arrow-left.svg'
 
 const CheckoutFooter = () => {
-    
     return (
         <div className='bg-gray-200 h-12 flex text-sm pt-3 pl-4 text-gray-800'>
-            <IconArrowLeft 
+            <IconArrowLeft
                 className='mr-2 text-base fill-current text-gray-800'
             />
             Back to shop
