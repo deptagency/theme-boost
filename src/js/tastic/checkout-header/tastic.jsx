3970a3978fb5a9f9f746859fa38706247fc01096
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-05-04 10:41:07 +0200

    made header checkout tastic

---
+++ src/js/tastic/checkout-header/tastic.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CheckoutHeader from 'Molecules/CheckoutHeader'
+
+const CheckoutHeaderTastic = () => {
+    return (
+        <CheckoutHeader />
+    )
+}
+
+export default CheckoutHeaderTastic
