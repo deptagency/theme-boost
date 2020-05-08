15b9413c05d0e3c609ba0d8305e2da51285de176
Sanja <codesanja@gmail.com>
2020-05-08 15:06:23 +0100

    Feature Service - small correction

--- src/js/patterns/organisms/FeatureService/index.jsx
+++ src/js/patterns/organisms/FeatureService/index.jsx
@@ -7,7 +7,7 @@ import { ReactComponent as IconLockClosed } from 'Icons/tailwind-icons/icon-lock
 
 const FeatureService = () => {
     return (
-        <div className='flex flex-col md:flex-row w-9/12 md:mx-auto py-5'>
+        <div className='flex flex-col w-full md:flex-row md:w-9/12 md:mx-auto p-5 md:py-5'>
             <div className='w-full h-12 flex items-center justify-start border-b border-solid md:border-b-0 md:border-r'>
                 <IconRocket className='inline-block mr-3 w-5 h-5 md:w-6 md:h-6 fill-current text-indigo-600' />
                 <span className='text-sm text-gray-800 leading-tight'>
