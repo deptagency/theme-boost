d8c6f6aa29981be5d68500a8b5d29bee35f4434e
Sanja <codesanja@gmail.com>
2020-05-08 11:10:32 +0100

    Footer position adjusting.

--- src/js/patterns/molecules/Layout/CheckoutPanels/Panels.jsx
+++ src/js/patterns/molecules/Layout/CheckoutPanels/Panels.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { stepObject } from './stepObject'
 
 import TinySlider from 'tiny-slider-react'
-
+import './index.scss'
 const Panels = forwardRef(({ steps, current, setCurrent, data }, ts) => {
     const goToPanel = (panel) => {
         ts.current.slider.goTo(panel)
@@ -15,44 +15,46 @@ const Panels = forwardRef(({ steps, current, setCurrent, data }, ts) => {
     }
 
     return (
-        <TinySlider
-            ref={ts}
-            settings={{
-                loop: false,
-                touch: false,
-                controls: false,
-                autoHeight: true,
-            }}
-            onIndexChanged={(ev) => {
-                setCurrent(ev.index)
-            }}
+        <div className='checkout-panels-slider relative min-h-full'>
+            <TinySlider
+                ref={ts}
+                settings={{
+                    loop: false,
+                    touch: false,
+                    controls: false,
+                    autoHeight: true,
+                }}
+                onIndexChanged={(ev) => {
+                    setCurrent(ev.index)
+                }}
             >
 
-            {steps.map(({ component: PanelComponent, name, checkoutDetails, setCheckoutDetails }, i) => {
-                return (
-                    <PanelComponent
-                        key={i}
-                        name={name}
-                        data={data}
-                        updateHeight={() => {
-                            updateSliderHeight()
-                        }}
-                        goToPanelIndex={panel => {
-                            goToPanel(panel)
-                        }}
-                        goToNextPanel={() => {
-                            goToPanel(current + 1)
-                        }}
-                        goToPreviousPanel={() => {
-                            goToPanel(current - 1)
-                        }}
-                        checkoutDetails={checkoutDetails}
-                        setCheckoutDetails={setCheckoutDetails}
-                    />
-                )
-            })}
+                {steps.map(({ component: PanelComponent, name, checkoutDetails, setCheckoutDetails }, i) => {
+                    return (
+                        <PanelComponent
+                            key={i}
+                            name={name}
+                            data={data}
+                            updateHeight={() => {
+                                updateSliderHeight()
+                            }}
+                            goToPanelIndex={panel => {
+                                goToPanel(panel)
+                            }}
+                            goToNextPanel={() => {
+                                goToPanel(current + 1)
+                            }}
+                            goToPreviousPanel={() => {
+                                goToPanel(current - 1)
+                            }}
+                            checkoutDetails={checkoutDetails}
+                            setCheckoutDetails={setCheckoutDetails}
+                        />
+                    )
+                })}
 
-        </TinySlider>
+            </TinySlider>
+        </div>
     )
 })
 
