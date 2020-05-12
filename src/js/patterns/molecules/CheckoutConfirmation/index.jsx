b8a296d189514d68cbbab774f26f24278c58d485
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-05-12 14:26:03 +0200

    added fixed width

--- src/js/patterns/molecules/CheckoutConfirmation/index.jsx
+++ src/js/patterns/molecules/CheckoutConfirmation/index.jsx
@@ -6,19 +6,17 @@ import app from 'frontastic-catwalk/src/js/app/app'
 
 const CheckoutConfirmation = () => {
     return (
-        <div className='text-center mt-10 md:mt-20'>
+        <div className='text-center mt-10 md:mt-20 m-auto'>
             <IconCheckmarkCircle className='w-20 h-20 inline-block text-teal-500 fill-current ' />
-            <div className='text-center text-gray-800 mt-4'>
-                <p className='text-2xl font-bold'>
-                    <FormattedMessage id='checkout.hooray' /> <br />
-                    <FormattedMessage id='checkout.wasConfirmed' />
+            <div className='text-center text-gray-800'>
+                <p className='text-2xl font-bold w-3/12 m-auto'>
+                    <FormattedMessage id='checkout.orderConfirmed' />
                 </p>
                 <p className='text-sm font-bold mt-2'>
                     Order ID: 12458
                 </p>
-                <p className='mt-3 text-sm'>
-                    <FormattedMessage id='checkout.orderConfirmation' /> <br />
-                    <FormattedMessage id='andInvoice.hooray' />
+                <p className='mt-3 text-sm m-auto w-1/3'>
+                    <FormattedMessage id='checkout.orderConfirmation' />
                     <span className='font-bold ml-1'>
                         thisemail@gmail.com
                     </span>
