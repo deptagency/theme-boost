90e4a72f39eef4a57ed68651b9ac58ebdf636b48
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-05-12 10:24:10 +0200

    added routing and translation

--- src/js/patterns/molecules/CheckoutError/index.jsx
+++ src/js/patterns/molecules/CheckoutError/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Button from 'Atoms/button'
 import { ReactComponent as IconEmotionSad } from 'Icons/tailwind-icons/icon-emotion-sad.svg'
+import { FormattedMessage } from 'react-intl'
+import app from 'frontastic-catwalk/src/js/app/app'
 
 const CheckoutError = () => {
     return (
@@ -8,12 +10,23 @@ const CheckoutError = () => {
             <IconEmotionSad className='w-20 h-20 inline-block text-gray-400 fill-current ' />
             <div className='text-center text-gray-800 mt-4'>
                 <p className='text-2xl font-bold'>
-                    Something <br /> went wrong <br /> during payment
+                    <FormattedMessage id='checkout.something' /> <br />
+                    <FormattedMessage id='checkout.wentWrong' />  <br />
+                    <FormattedMessage id='checkout.duringPayment' />
                 </p>
                 <p className='mt-3 text-sm'>
-                    Don't worry you haven't been charged, <br /> but you can always try again
+                    <FormattedMessage id='checkout.beenCharged' /> <br />
+                    <FormattedMessage id='checkout.youCan' />
                 </p>
-                <Button variant='btn-outline btn-outline-black mt-6'>Try again</Button>
+                <Button
+                    variant='btn-outline btn-outline-black mt-6'
+                    onClick={(event) => {
+                        event.preventDefault()
+                        app.getRouter().replace('Frontastic.Frontend.Master.Checkout.checkout')
+                    }}
+                >
+                    <FormattedMessage id='checkout.tryAgain' />
+                </Button>
             </div>
         </div>
     )
--- src/js/patterns/molecules/CheckoutError/stories.jsx
+++ src/js/patterns/molecules/CheckoutError/stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import CheckoutError from './index'
+import Button from 'Atoms/button'
+import { ReactComponent as IconEmotionSad } from 'Icons/tailwind-icons/icon-emotion-sad.svg'
 
 export default {
     title: 'Checkout Confirmation',
@@ -7,8 +8,17 @@ export default {
 
 export const checkoutError = () => {
     return (
-        <div>
-            <CheckoutError />
+        <div className='text-center mt-10 md:mt-20'>
+            <IconEmotionSad className='w-20 h-20 inline-block text-gray-400 fill-current ' />
+            <div className='text-center text-gray-800 mt-4'>
+                <p className='text-2xl font-bold'>
+                    Something <br /> went wrong <br /> during payment
+                </p>
+                <p className='mt-3 text-sm'>
+                    Don't worry you haven't been charged, <br /> but you can always try again
+                </p>
+                <Button variant='btn-outline btn-outline-black mt-6'>Try again</Button>
+            </div>
         </div>
     )
 }
