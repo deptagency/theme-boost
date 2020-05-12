5e37e154d138993a72470b7642d001e35b7a8202
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-05-12 11:04:00 +0200

    Added loader

---
+++ src/js/patterns/organisms/Loaders/FullCart/stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import FullCartLoader from './index'
+
+export default {
+    title: 'Loader full cart',
+}
+
+export const fullCart = () => {
+    return (
+        <div>
+            <FullCartLoader />
+        </div>
+    )
+}
