1594d85cafa0fd2ff5256ae7e8680a279683db40
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-06-05 12:36:01 +0200

    style brushups and added translations

--- src/js/patterns/organisms/Account/AccountOverview/index.jsx
+++ src/js/patterns/organisms/Account/AccountOverview/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 import { ReactComponent as HappyIcon } from 'Icons/tailwind-icons/icon-emotion-happy.svg'
 import { ReactComponent as CartIcon } from 'Icons/tailwind-icons/icon-cart.svg'
@@ -8,53 +9,57 @@ import { ReactComponent as UserIcon } from 'Icons/tailwind-icons/icon-user.svg'
 
 const AccountOverview = () => {
     return (
-        <div className='m-auto md:flex md:w-11/12 text-gray-800 md:h-full-84px'>
-            <div className='flex-auto md:shadow-lg md:order-2 md:mt-10 md:w-9/12 md:ml-4 lg:ml-5'>
-                <div className='flex flex-col text-center justify-center w-64 mx-auto my-5'>
-                    <div className='mx-auto mb-5'>
-                        <HappyIcon className='w-20 h-20 text-indigo-200 fill-current' />
-                    </div>
-                    <div>
-                        <div className='font-bold text-2xl mb-2'>
-                            Welcome Alexandra!
+        <>
+            <div className='m-auto md:flex md:w-11/12 text-gray-800 md:h-full-84px'>
+                <div className='flex-auto md:shadow-lg md:order-2 md:mt-10 md:w-7/12 md:ml-4 lg:ml-5'>
+                    <div className='flex flex-col text-center justify-center w-64 mx-auto my-5'>
+                        <div className='mx-auto mb-5'>
+                            <HappyIcon className='w-20 h-20 text-indigo-200 fill-current' />
                         </div>
-                        <div className='text-sm text-gray-600'>
-                            Here you can view your latest orders and change your account details.
+                        <div>
+                            <div className='font-bold text-2xl mb-2'>
+                                <FormattedMessage id='account.welcome' />
+                                Alexandra!
+                                {/* here will be instead of 'Alexandra' -> { session.account.firstName }, I suppose */}
+                            </div>
+                            <div className='text-sm text-gray-600'>
+                                <FormattedMessage id='account.latestDetails' />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex-auto mt-10 md:shadow-lg md:w-4/12'>   
-                <button className='flex mx-auto w-5/6 sm:w-4/6 justify-between border-b pb-4 mb-6'>
-                <div className='flex'>
-                    <CartIcon className='mr-2 text-2xl text-gray-500 fill-current'/>
-                    <div>
-                        Orders
-                    </div>
+                <div className='flex-auto mt-10 md:shadow-lg md:w-4/12'>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <CartIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                <FormattedMessage id='account.orders' />
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <HomeIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                <FormattedMessage id='account.addresses' />
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <UserIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                <FormattedMessage id='account.accountDetails' />
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
                 </div>
-                    <RightIcon className='md:hidden'/>
-                </button>
-                <button className='flex mx-auto w-5/6 sm:w-4/6 justify-between border-b pb-4 mb-6'>
-                <div className='flex'>
-                    <HomeIcon className='mr-2 text-2xl text-gray-500 fill-current'/>
-                    <div>
-                        Addresses
-                    </div>
-                </div>
-                    <RightIcon className='md:hidden'/>
-                </button>
-                <button className='flex mx-auto w-5/6 sm:w-4/6 justify-between border-b pb-4 mb-6'>
-                <div className='flex'>
-                    <UserIcon className='mr-2 text-2xl text-gray-500 fill-current'/>
-                    <div>
-                        Account Details
-                    </div>
-                </div>
-                    <RightIcon className='md:hidden'/>
-                </button>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/js/patterns/organisms/Account/AccountOverview/stories.jsx
+++ src/js/patterns/organisms/Account/AccountOverview/stories.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import AccountOverview from 'Organisms/Account/AccountOverview/index'
+import { ReactComponent as HappyIcon } from 'Icons/tailwind-icons/icon-emotion-happy.svg'
+import { ReactComponent as CartIcon } from 'Icons/tailwind-icons/icon-cart.svg'
+import { ReactComponent as RightIcon } from 'Icons/tailwind-icons/icon-chevron-right.svg'
+import { ReactComponent as HomeIcon } from 'Icons/tailwind-icons/icon-home.svg'
+import { ReactComponent as UserIcon } from 'Icons/tailwind-icons/icon-user.svg'
 
 export default {
     title: 'Account overview',
@@ -7,8 +11,54 @@ export default {
 
 export const accountOverview = () => {
     return (
-        <div>
-            <AccountOverview />
-        </div>
+        <>
+            <div className='m-auto md:flex md:w-11/12 text-gray-800 md:h-full-84px'>
+                <div className='flex-auto md:shadow-lg md:order-2 md:mt-10 md:w-7/12 md:ml-4 lg:ml-5'>
+                    <div className='flex flex-col text-center justify-center w-64 mx-auto my-5'>
+                        <div className='mx-auto mb-5'>
+                            <HappyIcon className='w-20 h-20 text-indigo-200 fill-current' />
+                        </div>
+                        <div>
+                            <div className='font-bold text-2xl mb-2'>
+                                Welcome Alexandra!
+                            </div>
+                            <div className='text-sm text-gray-600'>
+                                Here you can view your latest orders and change your account details.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex-auto mt-10 md:shadow-lg md:w-4/12'>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <CartIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                Orders
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <HomeIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                Addresses
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
+                    <button className='flex mx-auto w-5/6 sm:w-8/12 md:w-5/6 justify-between border-b pb-4 mb-6'>
+                        <div className='flex'>
+                            <UserIcon className='mr-2 text-2xl text-gray-500 fill-current' />
+                            <div>
+                                Account Details
+                            </div>
+                        </div>
+                        <RightIcon className='md:hidden' />
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
