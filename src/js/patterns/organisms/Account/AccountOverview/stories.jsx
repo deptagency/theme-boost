0d050c77c66f8214dcc4da5f2a9132d9bc16af39
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2020-06-05 10:29:02 +0200

    made accound overview markup

---
+++ src/js/patterns/organisms/Account/AccountOverview/stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AccountOverview from 'Organisms/Account/AccountOverview/index'
+
+export default {
+    title: 'Account overview',
+}
+
+export const accountOverview = () => {
+    return (
+        <div>
+            <AccountOverview />
+        </div>
+    )
+}
