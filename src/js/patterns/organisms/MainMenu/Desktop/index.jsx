29bfd43eeae9895c45e02443ccdf4d7a5e28634f
Marcel Fahle <m.fahle@gmail.com>
2020-07-30 14:19:45 +0300

    fix: removes more outside spacings
    also uses grid for consistency to achieve fullWidth

--- src/js/patterns/organisms/MainMenu/Desktop/index.jsx
+++ src/js/patterns/organisms/MainMenu/Desktop/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 
+import Grid from '@frontastic/catwalk/src/js/patterns/atoms/grid/grid'
+import Cell from '@frontastic/catwalk/src/js/patterns/atoms/grid/cell'
+
 import useBackgroundImageUrl from '@frontastic/catwalk/src/js/helper/hooks/useBackgroundImageUrl'
 import TopCategories from './TopCategories'
 import DesktopMenu from './Navigation'
@@ -35,51 +38,56 @@ const Desktop = ({
 
     return (
         <div className='hidden lg:block'>
-            <div className='o-wrapper'>
-                <div className='flex justify-between h-16'>
-                    <div className='inline-flex'>
-                        <a className='self-center w-32 mr-3' ref={ref} href={window.location.origin}>
-                            <img src={logoUrl} alt={logo.media.name} />
-                        </a>
+            <Grid>
+                <Cell fullWidth>
+                    <div className='o-wrapper'>
+                        <div className='flex justify-between h-16'>
+                            <div className='inline-flex'>
+                                <a className='self-center w-32 mr-3' ref={ref} href={window.location.origin}>
+                                    <img src={logoUrl} alt={logo.media.name} />
+                                </a>
 
-                        <TopCategories
-                            topCategories={topCategories}
-                            currentTopCategory={currentTopCategory}
-                            handleClick={(e, i) => {
-                                e.preventDefault()
-                                handleSelectTopCategory(i)
-                            }}
-                        />
+                                <TopCategories
+                                    topCategories={topCategories}
+                                    currentTopCategory={currentTopCategory}
+                                    handleClick={(e, i) => {
+                                        e.preventDefault()
+                                        handleSelectTopCategory(i)
+                                    }}
+                                />
+                            </div>
+                            <Widgets cartItemsCount={cartItemsCount} />
+                        </div>
                     </div>
-                    <Widgets cartItemsCount={cartItemsCount} />
-                </div>
-            </div>
-            <div className='border-t border-solid border-gray-200'>
-                <div className='o-wrapper'>
-                    <div
-                        className='relative col-span-2'
-                        onMouseLeave={() => {
-                            setIsExpanded(false)
-                        }}
-                    >
-                        <DesktopMenu
-                            currentTree={currentTree}
-                            handleClick={handleClick}
-                            onHoverItem={(item) => {
-                                setHoveredMenuItem(item)
-                                setIsExpanded(item.children && item.children.length > 0)
-                            }}
-                        />
 
-                        <NavigationExpansionPanel
-                            expanded={isExpanded}
-                            item={hoveredMenuItem}
-                            navPath={navPath}
-                            handleClick={handleClick}
-                        />
+                    <div className='border-t border-solid border-gray-200'>
+                        <div className='o-wrapper'>
+                            <div
+                                className='relative col-span-2'
+                                onMouseLeave={() => {
+                                    setIsExpanded(false)
+                                }}
+                            >
+                                <DesktopMenu
+                                    currentTree={currentTree}
+                                    handleClick={handleClick}
+                                    onHoverItem={(item) => {
+                                        setHoveredMenuItem(item)
+                                        setIsExpanded(item.children && item.children.length > 0)
+                                    }}
+                                />
+
+                                <NavigationExpansionPanel
+                                    expanded={isExpanded}
+                                    item={hoveredMenuItem}
+                                    navPath={navPath}
+                                    handleClick={handleClick}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </Cell>
+            </Grid>
         </div>
     )
 }
