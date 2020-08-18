7f19974ca99597d31ec39e4863d1f13679b1fdf1
Sanja <codesanja@gmail.com>
2020-08-18 12:58:25 +0100

    fix(boost-theme): header refactored, removed: Grid and Cell components

--- src/js/patterns/organisms/MainMenu/Desktop/index.jsx
+++ src/js/patterns/organisms/MainMenu/Desktop/index.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-import Grid from '@frontastic/catwalk/src/js/patterns/atoms/grid/grid'
-import Cell from '@frontastic/catwalk/src/js/patterns/atoms/grid/cell'
-
 import useBackgroundImageUrl from '@frontastic/catwalk/src/js/helper/hooks/useBackgroundImageUrl'
+import MarginBreakout from 'Molecules/Layout/MarginBreakout'
 import TopCategories from './TopCategories'
 import DesktopMenu from './Navigation'
 import NavigationExpansionPanel from './NavigationExpansionPanel'
@@ -41,62 +39,57 @@ const Desktop = ({
     }
 
     return (
-        <div className='hidden lg:block'>
-            <Grid>
-                <Cell fullWidth>
-                    <div className='o-wrapper'>
-                        <div className='flex justify-between h-16'>
-                            <div className='inline-flex'>
-                                <a className='self-center w-32 mr-6' ref={ref} href={window.location.origin}>
-                                    <img src={logoUrl} alt={logo.media.name} />
-                                </a>
-                                <TopCategories
-                                    topCategories={topCategories}
-                                    currentTopCategory={currentTopCategory}
-                                    handleClick={(e, i) => {
-                                        e.preventDefault()
-                                        handleSelectTopCategory(i)
-                                    }}
-                                />
-                            </div>
-                            <Widgets
-                                cartItemsCount={cartItemsCount}
-                                goToCartPage={goToCartPage}
-                                wishListLineItemsCount={wishListLineItemsCount}
-                                goToWishlistPage={goToWishlistPage}
-                                goToProfilePage={goToProfilePage}
-                            />
-                        </div>
-                    </div>
-                    <div className='border-t border-solid border-neutral-200'>
-                        <div className='o-wrapper'>
-                            <div
-                                className='relative col-span-2'
-                                onMouseLeave={() => {
-                                    setIsExpanded(false)
-                                }}
-                            >
-                                <DesktopMenu
-                                    currentTree={currentTree}
-                                    handleClick={handleClick}
-                                    onHoverItem={(item) => {
-                                        setHoveredMenuItem(item)
-                                        setIsExpanded(item.children && item.children.length > 0)
-                                    }}
-                                />
+        <MarginBreakout variant='hidden lg:block'>
+            <div className='flex justify-between h-16 o-wrapper'>
+                <div className='inline-flex'>
+                    <a className='self-center w-32 mr-3' ref={ref} href={window.location.origin}>
+                        <img src={logoUrl} alt={logo.media.name} />
+                    </a>
+                    <TopCategories
+                        topCategories={topCategories}
+                        currentTopCategory={currentTopCategory}
+                        handleClick={(e, i) => {
+                            e.preventDefault()
+                            handleSelectTopCategory(i)
+                        }}
+                    />
+                </div>
+                <Widgets
+                    cartItemsCount={cartItemsCount}
+                    goToCartPage={goToCartPage}
+                    wishListLineItemsCount={wishListLineItemsCount}
+                    goToWishlistPage={goToWishlistPage}
+                    goToProfilePage={goToProfilePage}
+                />
+            </div>
 
-                                <NavigationExpansionPanel
-                                    expanded={isExpanded}
-                                    item={hoveredMenuItem}
-                                    navPath={navPath}
-                                    handleClick={handleClick}
-                                />
-                            </div>
-                        </div>
+            <div className='border-t border-solid border-neutral-200'>
+                <div className='o-wrapper'>
+                    <div
+                        className='relative col-span-2'
+                        onMouseLeave={() => {
+                            setIsExpanded(false)
+                        }}
+                    >
+                        <DesktopMenu
+                            currentTree={currentTree}
+                            handleClick={handleClick}
+                            onHoverItem={(item) => {
+                                setHoveredMenuItem(item)
+                                setIsExpanded(item.children && item.children.length > 0)
+                            }}
+                        />
+
+                        <NavigationExpansionPanel
+                            expanded={isExpanded}
+                            item={hoveredMenuItem}
+                            navPath={navPath}
+                            handleClick={handleClick}
+                        />
                     </div>
-                </Cell>
-            </Grid>
-        </div>
+                </div>
+            </div>
+        </MarginBreakout>
     )
 }
 
