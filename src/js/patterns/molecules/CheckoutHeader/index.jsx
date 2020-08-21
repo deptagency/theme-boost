6ffa000c1abef89fc5207cfc5a16de8689caa63a
Marcel Fahle <m.fahle@gmail.com>
2020-08-21 23:46:34 +0300

    renames imports of FullPageWidthWrapper
    aka playing ping pong :D

--- src/js/patterns/molecules/CheckoutHeader/index.jsx
+++ src/js/patterns/molecules/CheckoutHeader/index.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
-import MarginBreakout from 'Molecules/Layout/MarginBreakout'
+import FullPageWidthWrapper from 'Molecules/Layout/FullPageWidthWrapper'
 import useBackgroundImageUrl from '@frontastic/catwalk/src/js/helper/hooks/useBackgroundImageUrl'
 import { ReactComponent as IconLockClosed } from 'Icons/tailwind-icons/icon-lock-closed.svg'
 
@@ -10,7 +10,7 @@ const CheckoutHeader = ({ logo }) => {
     const backgroundImageUrl = useBackgroundImageUrl(ref, logo)
 
     return (
-        <MarginBreakout variant='flex h-12 shadow-md'>
+        <FullPageWidthWrapper className='flex h-12 shadow-md'>
             <div
                 className='self-center h-6 w-full max-w-124px ml-4'
                 ref={ref}
@@ -23,7 +23,7 @@ const CheckoutHeader = ({ logo }) => {
                 <FormattedMessage id='cart.checkout' />
                 <IconLockClosed className='text-sm ml-1 fill-current text-neutral-900' />
             </div>
-        </MarginBreakout>
+        </FullPageWidthWrapper>
     )
 }
 
--- src/js/patterns/molecules/Footer/index.jsx
+++ src/js/patterns/molecules/Footer/index.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Translatable from '@frontastic/catwalk/src/js/component/translatable'
 
-import MarginBreakout from 'Molecules/Layout/MarginBreakout'
+import FullPageWidthWrapper from 'Molecules/Layout/FullPageWidthWrapper'
 import ContactInfo from './ContactInfo'
 import LinkList from './LinkList'
 import MetaNav from './MetaNav'
 import PaymentMethods from './PaymentMethods'
 
 const Footer = ({
-    title, links,
-    infoHeader, infoHeaderIcon, infoLinks,
-    aboutHeader, aboutHeaderIcon, aboutLinks,
-    contacHeader, phoneNumber, phoneNumberSubline, email, emailOverline,
+    title,
+    links,
+    infoHeader,
+    infoHeaderIcon,
+    infoLinks,
+    aboutHeader,
+    aboutHeaderIcon,
+    aboutLinks,
+    contacHeader,
+    phoneNumber,
+    phoneNumberSubline,
+    email,
+    emailOverline,
     paymentMethods,
-
 }) => {
     return (
-        <MarginBreakout variant='bg-neutral-800 text-neutral-200'>
+        <FullPageWidthWrapper className='bg-neutral-800 text-neutral-200'>
             <div className='hidden lg:flex mx-auto py-10 justify-between lg:w-9/12 xl:w-7/12'>
-                <LinkList
-                    header={<Translatable value={infoHeader} />}
-                    headerIcon={infoHeaderIcon}
-                    links={infoLinks}
-                />
+                <LinkList header={<Translatable value={infoHeader} />} headerIcon={infoHeaderIcon} links={infoLinks} />
 
                 <ContactInfo
                     header={<Translatable value={contacHeader} />}
@@ -39,16 +43,11 @@ const Footer = ({
                     links={aboutLinks}
                 />
 
-                <PaymentMethods
-                    methods={paymentMethods}
-                />
+                <PaymentMethods methods={paymentMethods} />
             </div>
 
-            <MetaNav
-                title={<Translatable value={title} />}
-                links={links}
-            />
-        </MarginBreakout>
+            <MetaNav title={<Translatable value={title} />} links={links} />
+        </FullPageWidthWrapper>
     )
 }
 
