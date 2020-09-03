ced4de9eb6011ccaee5875c3695be1be01eb851d
Kore Nordmann <mail@kore-nordmann.de>
2020-09-03 09:51:27 +0200

    fix: Use RemoteImage on PDP for optimized image delivery

--- src/js/patterns/organisms/Product/ProductImages/index.jsx
+++ src/js/patterns/organisms/Product/ProductImages/index.jsx
@@ -2,25 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Slider from 'Molecules/Slider'
 
+import RemoteImage from '@frontastic/catwalk/src/js/remoteImage'
+
 const ProductImages = ({ images }) => {
     return (
         <div className='mt-4 md:mt-6 bg-white rounded'>
             <Slider options={{ items: 1, loop: true, responsive: {} }}>
                 {images.map((image, i) => {
                     return (
-                        <div
+                        <RemoteImage
                             key={i}
-                            className='bg-white rounded'
-                            style={{
-                                backgroundImage: `url(${image})`,
-                                backgroundSize: 'contain',
-                                backgroundRepeat: 'no-repeat',
-                                backgroundPosition: 'center',
-                                width: '100%',
-                                minWidth: '288px',
-                                minHeight: '23.74rem',
-                            }}
-                        />
+                            url={image}
+                            cropRatio='4:3'
+                            itemProp='image'
+                            options={{ crop: 'pad', background: 'transparent' }} />
                     )
                 })}
             </Slider>
