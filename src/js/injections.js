da9e0b2cdcd41d6abbf25dfb6a130e0274456eef
Marcel Fahle <m.fahle@gmail.com>
2020-09-25 14:44:36 +0200

    fix: filename typo

--- src/js/injections.js
+++ src/js/injections.js
@@ -1,6 +1,6 @@
 import ComponentInjector from '@frontastic/catwalk/src/js/app/injector'
 
-import Notification from './patterns/molecules/Notifications/Default2'
+import Notification from './patterns/molecules/Notifications/Default'
 
 export default (() => {
     ComponentInjector.set('AtomsNotification', Notification)
