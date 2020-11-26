cd31ebbfd44d7212b8966d2861b973e46f1997f5
Marcel Fahle <m.fahle@gmail.com>
2020-11-26 16:31:35 +0100

    fix: adds missing key prop to StarRating

--- src/js/patterns/organisms/Product/StarRating.jsx
+++ src/js/patterns/organisms/Product/StarRating.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
 
 import { ReactComponent as IconStar } from 'Icons/tailwind-icons/icon-star.svg'
 import { ReactComponent as IconStarFull } from 'Icons/tailwind-icons/icon-star-full.svg'
 
 const StarRating = ({ numStars = 0, numReviews = 0, className = '' }) => {
-    const stars = new Array(5).fill(0).map((e, i) => (i < numStars ? <IconStarFull /> : <IconStar />))
+    const stars = new Array(5).fill(0).map((e, i) => (i < numStars ? <IconStarFull key={i} /> : <IconStar key={i} />))
 
     return (
         <div className={`flex items-center ${className}`}>
