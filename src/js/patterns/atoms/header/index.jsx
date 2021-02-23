fa14d8f8e96071df885a7a602159e6b82d3a2fde
Kore Nordmann <mail@kore-nordmann.de>
2021-02-23 10:36:41 +0100

    chore: CS

--- src/js/patterns/atoms/header/index.jsx
+++ src/js/patterns/atoms/header/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ComponentInjector from '@frontastic/catwalk/src/js/app/injector'
 
-export default function Header({ title, className = '' }) {
+const Header = ({ title, className = '' }) => {
     return <h2 className={`text-2xl text-gray-900 font-bold antialiased ${className}`}>{title}</h2>
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    className: PropTypes.string,
+}
+
+export default ComponentInjector.return('Header', Header)
